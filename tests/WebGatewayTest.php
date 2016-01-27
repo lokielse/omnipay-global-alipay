963c35f58b16f3c7aa3d7b12123e34d2b76cb8e1
QS <wic@qq.com>
2016-01-28 01:41:20 +0800

    Add tests

---
+++ tests/WebGatewayTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Omnipay\GlobalAlipay;
+
+use Omnipay\GlobalAlipay\Message\CompletePurchaseResponse;
+use Omnipay\GlobalAlipay\Message\WebPurchaseResponse;
+use Omnipay\Omnipay;
+use Omnipay\Tests\GatewayTestCase;
+
+class WebGatewayTest extends GatewayTestCase
+{
+
+    /**
+     * @var WebGateway $gateway
+     */
+    protected $gateway;
+
+    protected $options;
+
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->gateway = Omnipay::create('GlobalAlipay_Web');
+        $this->gateway->setPartner('123456');
+        $this->gateway->setKey('xxxxxxx');
+        $this->gateway->setSignType('MD5');
+        $this->gateway->setNotifyUrl('http://example.com/notify');
+
+    }
+
+
+    public function testPurchase()
+    {
+        $order = array (
+            'subject'      => 'test', //Your order ID
+            'out_trade_no' => date('YmdHis'), //Should be format 'YmdHis'
+            'total_fee'    => '0.01', //Order Title
+        );
+
+        /**
+         * @var WebPurchaseResponse $response
+         */
+        $response = $this->gateway->purchase($order)->send();
+        $this->assertTrue($response->isSuccessful());
+        $this->assertTrue($response->isRedirect());
+        $this->assertNotEmpty($response->getRedirectData());
+    }
+
+
+    public function testCompletePurchase()
+    {
+        $options = array (
+            'request_params' => array (
+                'out_trade_no' => '123456',
+                'sign'         => '123456',
+            ),
+        );
+
+        /**
+         * @var CompletePurchaseResponse $response
+         */
+        $response = $this->gateway->completePurchase($options)->send();
+        $this->assertFalse($response->isSuccessful());
+    }
+}

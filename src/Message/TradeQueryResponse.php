54675a6db477d905f6cadcaba22622c92119915a
lisida <lisida@linlaoban.com>
2018-08-04 17:00:54 +0800

    add trade query

---
+++ src/Message/TradeQueryResponse.php
@@ -0,0 +1,20 @@
+<?php
+namespace Omnipay\GlobalAlipay\Message;
+use Omnipay\Common\Message\AbstractResponse;
+class TradeQueryResponse extends AbstractResponse
+{
+    /**
+     * Is the response successful?
+     *
+     * @return boolean
+     */
+    public function isSuccessful()
+    {
+        return $this->isPaid();
+    }
+    public function isPaid()
+    {
+        $data = $this->getData();
+        return $data['paid'];
+    }
+}
